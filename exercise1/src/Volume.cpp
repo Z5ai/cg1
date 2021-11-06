6a1dd389810a63fcedc4da062aa6a48f5da522c2
david <david.nicklaser@mailbox.tu-dresden.de>
2021-11-06 22:40:50 +0100

    return of value calculation must still be verified

--- exercise1/src/Volume.cpp
+++ exercise1/src/Volume.cpp
@@ -8,8 +8,18 @@
 
 float ComputeVolume(const HEMesh& m)
 {
-	float vol = 0;
-	/*Task 1.2.2*/
-	std::cout << "Volume calculation is not implemented." << std::endl;
-	return vol;
+    float vol = 0;
+    for (auto f_it=m.faces_begin(); f_it!=m.faces_end(); ++f_it){ // iterate all faces
+
+        HEMesh::ConstFaceVertexIter fv_it = m.cfv_iter(*f_it); // vector_iter fh_it is created based on the current face *f_it
+        OpenMesh::Vec3f vec0 = m.point(*(fv_it++)); // could this also be done by v = *f_it.vertices; TripleProduct = f(v[0],v[1],v[2]);?
+        OpenMesh::Vec3f vec1 = m.point(*(fv_it++));
+        OpenMesh::Vec3f vec2 = m.point(*(fv_it++));
+        // probably throw an error or message, if one face has more vertices than 3
+        // if calculation is done with cube: "Triangulating mesh." - message is displayed in console. afterwards the area and volume is different than before.
+        auto triple_product = abs(OpenMesh::dot(vec0, OpenMesh::cross(vec1, vec2)));
+
+        vol += triple_product;
+    }
+    return vol/6;
 }
