eb7eba5700574b9651b0f75842b3a3c7fef41987
Alienmiep <alienmiep@gmx.de>
2021-11-19 20:42:14 +0100

    Added 1_2_3 pseudocode

--- exercise1/src/ShellExtraction.cpp
+++ exercise1/src/ShellExtraction.cpp
@@ -8,10 +8,40 @@
 
 unsigned int ExtractShells(HEMesh& m, OpenMesh::FPropHandleT<int> perFaceShellIndex)
 {
-	//reset the shell indices to -1 for every face
-	for (auto f : m.faces())
-		m.property(perFaceShellIndex, f) = -1;
+    //reset the shell indices to -1 for every face
+    for (auto f : m.faces())
+        m.property(perFaceShellIndex, f) = -1;
 
-	/*Task 1.2.3*/
-	return 0;
+    /*Task 1.2.3*/
+
+    /* region-grow pseudocode
+    unsigned int num_components = 0;
+    std::vector<OpenMesh::FaceHandle> all_faces; //pool of all faces with id -1
+    std::vector<OpenMesh::FaceHandle> neighbors; //pool of neighboring faces
+
+     foreach(face){
+        add to all_faces
+     }
+
+     if(all_faces.empty(){
+        return 0;
+     }
+
+    while(all_faces not empty){
+    current_face = take one out of all_faces (pop?)
+     do {
+        assign num_components to current_face (as shell_id)
+        foreach(current_face adjacent faces)        //add its neighbors with id -1 to neighbors (if not already there)
+            if(shell_id == -1 && not in neighbors yet) {
+                add to neighbors
+                remove from all_faces
+            }
+        remove current_face from neighbors
+    } while(!neighbors.empty())
+
+    num_components+=1;
+    } */
+
+
+    return num_components;
 }
