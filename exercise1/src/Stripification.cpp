d1554b7b71421ce9d868e1ded1b9c87e0c3b194b
Alienmiep <alienmiep@gmx.de>
2021-11-24 13:16:10 +0100

    Started 1_2_5

--- exercise1/src/Stripification.cpp
+++ exercise1/src/Stripification.cpp
@@ -13,7 +13,7 @@
 unsigned int ExtractTriStrips(HEMesh& mesh, OpenMesh::FPropHandleT<int> perFaceStripIdProperty, unsigned int nTrials)
 {
 	//prepare random engine
-	std::mt19937 eng;
+	std::mt19937 eng;            //properly initialize later
 
 	//initialize strip index to -1 for each face
 	for (auto f : mesh.faces())
@@ -23,13 +23,92 @@ unsigned int ExtractTriStrips(HEMesh& mesh, OpenMesh::FPropHandleT<int> perFaceS
 
 	/*Task 1.2.5*/
 
+    std::vector<OpenMesh::FaceHandle> remaining_faces;
+    std::vector<OpenMesh::HalfedgeHandle> trial;
+    std::vector<OpenMesh::HalfedgeHandle> trial_max;
+    OpenMesh::FaceHandle starting_face;
+    OpenMesh::HalfedgeHandle current_he;
 
-    stripification pseudocode
-    k from the lecture = nTrials
+    OpenMesh::HPropHandleT<int> perHalfedgeParityProperty;
 
-    while(faces with id -1 exist){
-    for(int i = 0; i < nTrials; i++){
-        randomly select a halfedge hi with attribute id -1
+    for(auto h : mesh.halfedges()) {
+        mesh.property(perHalfedgeParityProperty, h) = -1;
+    }
+
+    for(auto f : mesh.faces()){
+        remaining_faces.push_back(f);
+    }
+
+    while(!remaining_faces.empty()){
+        trial_max.clear();
+        //grab 20 random faces out of pool
+        for(int i = 0; i < nTrials; i++) {
+            trial.clear();
+            std::uniform_int_distribution<> randomNumber{0, static_cast<int>((remaining_faces.size()-1))};
+            starting_face = remaining_faces[randomNumber(eng)];
+
+            HEMesh::ConstFaceHalfedgeIter cfh_it = mesh.cfh_iter(starting_face);
+            OpenMesh::HalfedgeHandle starting_he = *cfh_it;
+            trial.push_back(starting_he);
+            mesh.property(perHalfedgeParityProperty, starting_he) = 0;
+
+            current_he = starting_he;
+            //iterate forwards
+            switch(mesh.property(perHalfedgeParityProperty, current_he)) {
+                case 0:
+                    //current_he = prev(inv(current_he));
+                    current_he = mesh.opposite_halfedge_handle(current_he);
+                    current_he = mesh.prev_halfedge_handle(current_he);
+                    //add current to list
+                    mesh.property(perHalfedgeParityProperty,current_he) = 1;
+                    trial.push_back(current_he);
+                    break;
+                case 1:
+                    //current_he = next(inv(current_he));
+                    current_he = mesh.opposite_halfedge_handle(current_he);
+                    current_he = mesh.next_halfedge_handle(current_he);
+                    //add current to list
+                    mesh.property(perHalfedgeParityProperty,current_he) = 0;
+                    trial.push_back(current_he);
+                    break;
+            }
+
+            current_he = starting_he;
+            //iterate backwards
+            switch(mesh.property(perHalfedgeParityProperty, current_he)) {
+                case 0:
+                    //current_he = prev(inv(current_he));
+                    current_he = mesh.prev_halfedge_handle(current_he);
+                    current_he = mesh.opposite_halfedge_handle(current_he);
+                    //add current to list
+                    mesh.property(perHalfedgeParityProperty,current_he) = 1;
+                    trial.push_back(current_he);
+                    break;
+                case 1:
+                    //current_he = next(inv(current_he));
+                    current_he = mesh.next_halfedge_handle(current_he);
+                    current_he = mesh.opposite_halfedge_handle(current_he);
+                    //add current to list
+                    mesh.property(perHalfedgeParityProperty,current_he) = 0;
+                    trial.push_back(current_he);
+                    break;
+            }
+        if(trial.size() > trial_max.size()) {
+            trial_max = trial;
+        }
+
+
+
+        }
+
+    }
+
+    //stripification pseudocode
+    //k from the lecture = nTrials
+
+   //while(faces with id -1 exist){
+    //for(int i = 0; i < nTrials; i++){
+        //randomly select a halfedge hi with attribute id -1
         make a strip out of it, forwards and backwards
             hi.parity = 0       //give current halfedge parity 0
             add hi to list of halfedges
